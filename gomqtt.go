7ae1a37820e5f9b4f4321625011205cd740ecc23
Martin Rauscher <hades32@gmail.com>
2016-01-24 23:18:06 +0100

    improve logging and force disconnect to speed it up

--- gomqtt.go
+++ gomqtt.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"strings"
 	"sync"
@@ -13,6 +14,7 @@ import (
 )
 
 var (
+	debugEnabled           bool
 	ignoreRetained         bool
 	insecure               bool
 	ignorePayload          bool
@@ -22,7 +24,8 @@ var (
 	pubTopic               string
 	pubMessage             string
 	numberMessagesExpected int
-  qos                    byte
+	qos                    byte
+  logger = log.New(os.Stderr, "", log.Ldate | log.Ltime | log.Lmicroseconds)
 )
 
 func main() {
@@ -32,31 +35,18 @@ func main() {
 
 	var wg sync.WaitGroup
 	setupSubscriptions(client, &wg)
-  publishMessage(client)
+	publishMessage(client)
 	wg.Wait()
 
-  client.Disconnect(1)
-	log("done.")
-}
-
-func publishMessage(client *mqtt.Client) {
-  if pubTopic != "" {
-    token := client.Publish(pubTopic, qos, false, pubMessage)
-    token.WaitTimeout(10 * time.Second)
-    if token.Error() != nil {
-      logF("FATAL: Could not publish: %v", token.Error())
-      os.Exit(-1)
-    }
-  } else {
-    log("nothing to publish")
-  }
+	info("done.")
 }
 
 func parseArgs() {
-  var qosInt int
-	flag.BoolVar(&ignoreRetained, "ignore-retained", false, "if TRUE, will only consider live (non-retained) messages")
-	flag.BoolVar(&insecure, "insecure", false, "if TRUE, will allow TLS/SSL connections without certificate and hostname validation")
-	flag.BoolVar(&ignorePayload, "ignore-payload", false, "if TRUE, will only print a summary line per message")
+	var qosInt int
+	flag.BoolVar(&debugEnabled, "debug", false, "if set, display debug messages")
+	flag.BoolVar(&ignoreRetained, "ignore-retained", false, "if set, will only consider live (non-retained) messages")
+	flag.BoolVar(&insecure, "insecure", false, "if set, will allow TLS/SSL connections without certificate and hostname validation")
+	flag.BoolVar(&ignorePayload, "ignore-payload", false, "if set, will only print a summary line per message")
 	flag.StringVar(&serverURL, "url", "tcp://localhost:1883", "the server url to connect to")
 	flag.StringVar(&topicFiltersString, "sub", "", "the topic(s) to subscribe to (may be a comma separated list)")
 	flag.StringVar(&clientID, "clientid", "", "the mqtt clientid to use (optional)")
@@ -65,7 +55,14 @@ func parseArgs() {
 	flag.IntVar(&numberMessagesExpected, "msg-count", 1, "number of messages to receive before exitting")
 	flag.IntVar(&qosInt, "qos", 0, "QoS for publishes and subscriptions")
 	flag.Parse()
-  qos = byte(qosInt)
+	qos = byte(qosInt)
+
+  if debugEnabled {
+    mqtt.DEBUG = logger
+    mqtt.WARN = logger
+    mqtt.ERROR = logger
+    mqtt.CRITICAL = logger
+  }
 }
 
 func createMqttOptsFromFlags() *mqtt.ClientOptions {
@@ -79,23 +76,23 @@ func createMqttOptsFromFlags() *mqtt.ClientOptions {
 }
 
 func connect(opts *mqtt.ClientOptions) *mqtt.Client {
-	log("Starting mqtt client")
+	info("Starting mqtt client")
 	client := mqtt.NewClient(opts)
 	token := client.Connect()
 	if token.Error() != nil {
-		logF("FATAL: Could not connect: %v", token.Error())
+		infoF("FATAL: Could not connect: %v", token.Error())
 		os.Exit(-1)
 	}
 	token.WaitTimeout(10 * time.Second)
 	if token.Error() != nil {
-		logF("FATAL: Could not connect: %v", token.Error())
+		infoF("FATAL: Could not connect: %v", token.Error())
 		os.Exit(-1)
 	}
 	if !client.IsConnected() {
-		log("FATAL: Not connected after timeout!")
+		info("FATAL: Not connected after timeout!")
 		os.Exit(-1)
 	}
-	log("connected")
+	info("connected")
 	return client
 }
 
@@ -113,26 +110,39 @@ func setupSubscriptions(client *mqtt.Client, wg *sync.WaitGroup) {
 					if ignorePayload {
 						fmt.Printf("r:%v, t:%v, s:%v\n", msg.Retained(), msg.Topic(), len(payload))
 					} else {
-						logF("r:%v, t:%v, s:%v", msg.Retained(), msg.Topic(), len(payload))
+						infoF("r:%v, t:%v, s:%v", msg.Retained(), msg.Topic(), len(payload))
 						fmt.Printf("%v\n", payload)
 					}
 					wg.Done()
 				} else {
-					log("ignoring retained msg")
+					info("ignoring retained msg")
 				}
 			})
 		}
 
-		logF("waiting for %v msgs.", numberMessagesExpected)
+		infoF("waiting for %v msgs.", numberMessagesExpected)
 	} else {
-		log("No subscriptions...")
+		info("No subscriptions...")
 	}
 }
 
-func log(msg string) {
-	fmt.Fprintln(os.Stderr, msg)
+func publishMessage(client *mqtt.Client) {
+	if pubTopic != "" {
+		token := client.Publish(pubTopic, qos, false, pubMessage)
+		token.WaitTimeout(10 * time.Second)
+		if token.Error() != nil {
+			infoF("FATAL: Could not publish: %v", token.Error())
+			os.Exit(-1)
+		}
+	} else {
+		info("nothing to publish")
+	}
 }
 
-func logF(format string, a ...interface{}) {
-	fmt.Fprintf(os.Stderr, format+"\n", a...)
+func info(msg string) {
+  logger.Println(msg)
+}
+
+func infoF(format string, a ...interface{}) {
+	logger.Printf(format+"\n", a...)
 }
